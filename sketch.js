07804075fc7727e7cc414f60ba21510d7f7186db
My Tran(Lisa) Ho <mt2ho@uwaterloo.ca>
2026-01-08 12:29:18 -0500

    Update sketch.js
    changed background to purple, add text

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
   createCanvas(400, 400);
+  textSize(24); // size of the text
+  textAlign(CENTER); // center the text
 }
 
 function draw() {
-  background(220);
+  background(204, 204, 255);
+
+  fill(0); // text color (black)
+  text("Is This Seat Taken?!", width / 2, height / 2);
 }
